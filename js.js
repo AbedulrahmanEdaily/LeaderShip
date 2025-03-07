b9015c71c2397b22da92318cab17cac5b54c94db
Abedulrahman Edaily <dylyb7883@gmail.com>
2025-03-08 01:36:16 +0200

    The Final Update

--- js.js
+++ js.js
@@ -13,14 +13,9 @@ let dayConten = document.querySelectorAll(".day-content");
 
 clickday.forEach((day, i) => {
     day.addEventListener("click", function () {
-        // Remove active class from previous active day
         document.querySelector(".day.active")?.classList.remove("active");
         day.classList.add("active");
-
-        // Hide previously shown content
         document.querySelector(".day-content.show")?.classList.remove("show");
-
-        // Show the corresponding content
         dayConten.forEach((content) => {
             if (Number(content.getAttribute("day")) === i + 1) {
                 content.classList.add("show");
@@ -32,27 +27,19 @@ clickday.forEach((day, i) => {
 
 const navLinks = document.querySelectorAll('.nav-item');
 
-// مراقبة التمرير
 window.addEventListener('scroll', () => {
     let currentSection = '';
-
     navLinks.forEach(link => {
         const section = document.querySelector(link.getAttribute('data-target'));
         const sectionTop = section.offsetTop;
         const sectionHeight = section.offsetHeight;
-
-
         if (window.scrollY >= sectionTop - sectionHeight / 3 && window.scrollY < sectionTop + sectionHeight) {
             currentSection = link.getAttribute('data-target');
         }
     });
-
-
     navLinks.forEach(link => {
         link.classList.remove('active');
     });
-
-
     if (currentSection) {
         const currentLink = Array.from(navLinks).find(link => link.getAttribute('data-target') === currentSection);
         if (currentLink) {
